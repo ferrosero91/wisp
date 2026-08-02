dc76a0718c0254a28cb5a4e4637be060ea0b9e63
ferrosero91 <ferrosero91@gmail.com>
2026-08-02 16:04:31 -0500

    style: redesign professional login page and update receipt templates

--- Assets/js/functions/login.js
+++ Assets/js/functions/login.js
@@ -114,10 +114,10 @@ function modal(){
 particlesJS({
 	"particles": {
 		"number": {
-				"value": 150,
+				"value": 60,
 				"density": {
 						"enable": true,
-						"value_area": 800
+						"value_area": 1000
 				}
 		},
 		"color": {
@@ -125,99 +125,46 @@ particlesJS({
 		},
 		"shape": {
 				"type": "circle",
-				"stroke": {
-						"width": 0,
-						"color": "#000000"
-				},
-				"polygon": {
-						"nb_sides": 5
-				},
-				"image": {
-						"src": "img/github.svg",
-						"width": 100,
-						"height": 100
-				}
+				"stroke": { "width": 0, "color": "#000000" },
+				"polygon": { "nb_sides": 5 },
+				"image": { "src": "", "width": 100, "height": 100 }
 		},
 		"opacity": {
-				"value": 0.5,
-				"random": false,
-				"anim": {
-						"enable": false,
-						"speed": 1,
-						"opacity_min": 0.1,
-						"sync": false
-				}
+				"value": 0.2,
+				"random": true,
+				"anim": { "enable": false }
 		},
 		"size": {
-				"value": 3,
+				"value": 2,
 				"random": true,
-				"anim": {
-						"enable": false,
-						"speed": 40,
-						"size_min": 0.1,
-						"sync": false
-				}
+				"anim": { "enable": false }
 		},
 		"line_linked": {
 				"enable": true,
-				"distance": 150,
+				"distance": 180,
 				"color": "#ffffff",
-				"opacity": 0.4,
+				"opacity": 0.12,
 				"width": 1
 		},
 		"move": {
 				"enable": true,
-				"speed": 11.22388442605866,
+				"speed": 0.6,
 				"direction": "none",
 				"random": false,
 				"straight": false,
 				"out_mode": "out",
 				"bounce": false,
-				"attract": {
-						"enable": false,
-						"rotateX": 600,
-						"rotateY": 1200
-				}
+				"attract": { "enable": false }
 		}
 	},
 	"interactivity": {
 		"detect_on": "canvas",
 		"events": {
-				"onhover": {
-						"enable": false,
-						"mode": "repulse"
-				},
-				"onclick": {
-						"enable": true,
-						"mode": "push"
-				},
+				"onhover": { "enable": false },
+				"onclick": { "enable": false },
 				"resize": true
 		},
-		"modes": {
-				"grab": {
-						"distance": 400,
-						"line_linked": {
-								"opacity": 1
-						}
-				},
-				"bubble": {
-						"distance": 400,
-						"size": 40,
-						"duration": 2,
-						"opacity": 8,
-						"speed": 3
-				},
-				"repulse": {
-						"distance": 200,
-						"duration": 0.4
-				},
-				"push": {
-						"particles_nb": 4
-				},
-				"remove": {
-						"particles_nb": 2
-				}
-		}
+		"modes": {}
 	},
 	"retina_detect": true
 })
--- Views/Login/login.php
+++ Views/Login/login.php
@@ -22,10 +22,7 @@
             $favicon = base_style().'/images/logotypes/favicon.png';
           }
         ?>
-        <!-- ================== INICIO ICONO ================== -->
         <link rel="icon" type="image/x-icon" href="<?= $favicon ?>">
-        <!-- ================== FIN ICONO ===================== -->
-    	<!-- ================== INICIO ARCHIVOS CSS =========== -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap">
@@ -34,10 +31,7 @@
         <link rel="stylesheet" href="<?= base_style() ?>/bookstores/gritter/css/jquery.gritter.css"/>
         <link rel="stylesheet" href="<?= base_style() ?>/css/login.css">
         <link rel="stylesheet" href="<?= base_style() ?>/css/login-modern.css">
-        <!-- ================== FIN ARCHIVOS CSS ============== -->
-        <!-- ================== INICIO TITULO ================= -->
         <title><?= $data['page_name'] ?></title>
-        <!-- ================== FIN TITULO =================== -->
     </head>
     <body class="pace-top">
         <div id="loading"><span class="loading-spinner"></span></div>
@@ -77,31 +71,30 @@
                                 $logo = base_style().'/images/logotypes/superwisp_white.png';
                             }
                         ?>
-                        <img src="<?= $logo ?>" class="img-responsive" style="max-width:230px; height:auto">
-    				</div>
-    				<div class="icon">
-    					<i class="fa fa-sign-in-alt"></i>
+                        <img src="<?= $logo ?>" class="img-responsive" alt="Logo">
     				</div>
     			</div>
     			<div class="login-content">
 				<form name="transactions" id="transactions" autocomplete="off" class="margin-bottom-0">
 					<input type="hidden" name="csrf_token" value="<?= $data['csrf_token'] ?>">
-					<div class="form-group m-b-20">
-						<input type="text" class="form-control form-control-lg" placeholder="Usuario" id="username" name="username" value="<?php if(isset($_COOKIE["username"])){ echo $_COOKIE["username"];} ?>">
+					<div class="form-group">
+						<i class="fa fa-user field-icon"></i>
+						<input type="text" class="form-control" placeholder="Usuario" id="username" name="username" value="<?php if(isset($_COOKIE["username"])){ echo $_COOKIE["username"];} ?>">
     					</div>
-    					<div class="form-group mb-0">
-    						<input type="password" class="form-control form-control-lg" placeholder="Contraseña" id="password" name="password" autocomplete="current-password">
+    					<div class="form-group">
+    						<i class="fa fa-lock field-icon"></i>
+    						<input type="password" class="form-control" placeholder="Contrase&ntilde;a" id="password" name="password" autocomplete="current-password">
                 <i class="fa fa-eye-slash showHidePw"></i>
     					</div>
-              <div class="checkbox checkbox-css m-b-20">
+              <div class="checkbox checkbox-css">
                 <input type="checkbox" id="remember" name="remember" <?php if(isset($_COOKIE["username"])){ ?> checked <?php } ?>>
-                <label for="remember">Mantener sesion</label>
+                <label for="remember">Mantener sesi&oacute;n</label>
               </div>
     					<div class="login-buttons">
-    						<button type="submit" class="btn btn-success btn-block btn-lg">Ingresar</button>
+    						<button type="submit" class="btn btn-block">Ingresar</button>
     					</div>
     					<div class="m-t-5">
-    						¿Olvidaste tu contraseña? <a href="javascript:;" onclick="modal();">Click aqui</a>.
+    						&iquest;Olvidaste tu contrase&ntilde;a? <a href="javascript:;" onclick="modal();">Click aqu&iacute;</a>
     					</div>
     				</form>
     			</div>
@@ -110,10 +103,7 @@
         <?php
           modal("loginModal",$data);
         ?>
-        <!-- ================== INICIO RUTA  ============== -->
         <script> const base_url = "<?= base_url(); ?>"; </script>
-        <!-- ================== INICIO RUTA  ============== -->
-        <!-- ================== INICIO ARCHIVOS JS ======== -->
         <script src="<?= base_style() ?>/js/app.min.js"></script>
         <script src="<?= base_style() ?>/js/particles.min.js"></script>
         <script src="<?= base_style() ?>/js/functions.js"></script>
@@ -121,6 +111,5 @@
         <script src="<?= base_style() ?>/bookstores/parsleyjs/parsley.js"></script>
         <script src="<?= base_style() ?>/bookstores/gritter/js/jquery.gritter.min.js"></script>
         <script src="<?= base_style() ?>/js/functions/<?= $data['page_functions_js']; ?>"></script>
-        <!-- ================== FIN ARCHIVOS JS =========== -->
     </body>
 </html>
